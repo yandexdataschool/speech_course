57d81940397db7266d3e43d5ae7b070f89766806
LiableFish <liablefish@gmail.com>
2026-02-15 21:27:27 +0200

    hw

--- week_01_DSP/transforms.py
+++ week_01_DSP/transforms.py
@@ -22,26 +22,37 @@ class Windowing:
         self.hop_length = hop_length if hop_length else self.window_size // 2
     
     def __call__(self, waveform):
-        left_padding = np.zeros(self.window_size // 2)
-        right_padding = np.zeros(self.window_size // 2)
-        effective_waveform = np.concat([left_padding, waveform, right_padding], axis=-1)
+        # left_padding = np.zeros(self.window_size // 2)
+        # right_padding = np.zeros(self.window_size // 2)
+        # effective_waveform = np.concat([left_padding, waveform, right_padding], axis=-1)
 
-        windows = []
-        for start in range(0, effective_waveform.shape[-1] - self.window_size + 1, self.hop_length):
-            end = start + self.window_size
-            windows.append(effective_waveform[start:end])
+        effective_waveform = np.pad(
+            waveform, 
+            (self.window_size // 2, self.window_size // 2),
+            mode='constant'
+        )
+        
+        # windows = []
+        # for start in range(0, effective_waveform.shape[-1] - self.window_size + 1, self.hop_length):
+        #     end = start + self.window_size
+        #     windows.append(effective_waveform[start:end])
 
-        return np.stack(windows)
+        # return np.stack(windows)
+
+        starts = np.arange(0, effective_waveform.shape[-1] - self.window_size + 1, self.hop_length)
+        offsets = np.arange(self.window_size)
+        indics = starts[:, None] + offsets
+        return effective_waveform[indics]
     
 
 class Hann:
     def __init__(self, window_size=1024):
-        self._sliding_window = scipy.signal.windows.hann(M=window_size, sym=False)
+        self.sliding_window = scipy.signal.windows.hann(M=window_size, sym=False)
     
     def __call__(self, windows):
         # windows (seq_len, window_length)
         # _sliding_window (window_length,)
-        return windows * self._sliding_window[None, :]
+        return windows * self.sliding_window[None, :]
 
 
 
@@ -64,23 +75,23 @@ class Square:
 
 class Mel:
     def __init__(self, n_fft, n_mels=80, sample_rate=22050):
-        self._mel_filterbank = librosa.filters.mel(
+        self.mel_filterbank = librosa.filters.mel(
             sr=sample_rate,
             n_fft=n_fft, 
             n_mels=n_mels,
             fmin=1,
             fmax=8192,
         )  # (n_mels, 1 + n_fft/2)
-        self._inverse_mel_filterbank = np.linalg.pinv(self._mel_filterbank)
+        self.inverse_mel_filterbank = np.linalg.pinv(self.mel_filterbank)
 
     def __call__(self, spec):
         # spec (n_frames, 1 + n_fft/2)
-        mel = spec @ self._mel_filterbank.T
+        mel = spec @ self.mel_filterbank.T
         return mel
 
     def restore(self, mel):
         # mel (n_frames, n_mels)
-        spec = mel @ self._inverse_mel_filterbank.T
+        spec = mel @ self.inverse_mel_filterbank.T
         return spec
 
 
@@ -134,114 +145,77 @@ class Wav2Mel:
 
 class TimeReverse:
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        return mel[::-1, :]
 
 
 class Loudness:
     def __init__(self, loudness_factor):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        self.loudness_factor = loudness_factor
 
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        return mel * self.loudness_factor
 
 
 
 class PitchUp:
     def __init__(self, num_mels_up):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        self.num_mels_up = num_mels_up
 
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        shifted = np.zeros_like(mel)
+        shift = max(mel.shape[1] - self.num_mels_up, 0)
+        shifted[:, -shift:] = mel[:, :shift]
+        return shifted
 
 
 class PitchDown:
     def __init__(self, num_mels_down):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        self.num_mels_down = num_mels_down
 
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        shifted = np.zeros_like(mel)
+        shift = max(mel.shape[1] - self.num_mels_down, 0)
+        shifted[:, :shift] = mel[:, -shift:]
+        return shifted
 
 
 class SpeedUpDown:
     def __init__(self, speed_up_factor=1.0):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        self.speed_up_factor = speed_up_factor
 
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        target_n_frames = int(self.speed_up_factor * mel.shape[0])
+        new_idx = np.round(np.arange(target_n_frames) / self.speed_up_factor).astype(np.int64)
+        new_idx = np.clip(new_idx, 0, mel.shape[0] - 1)
+        return mel[new_idx]
 
 
 class FrequenciesSwap:
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        return mel[:, ::-1]
 
 
 class WeakFrequenciesRemoval:
     def __init__(self, quantile=0.05):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        self.quantile = quantile
 
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        return np.where(mel < np.quantile(mel, q=self.quantile), 0, mel)
 
 
 class Cringe1:
-    def __init__(self):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
-
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        return 1 / mel
 
 
 class Cringe2:
-    def __init__(self):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
-
     def __call__(self, mel):
-        # Your code here
-        raise NotImplementedError("TODO: assignment")
-        # ^^^^^^^^^^^^^^
-
+        np.random.seed(123)
+        mel = mel.copy()
+        n_i = np.random.randint(mel.shape[0])
+        i = np.random.choice(mel.shape[0], size=n_i)
+        mel[i] = 0
+        n_j = np.random.randint(mel.shape[1])
+        j = np.random.choice(mel.shape[1], size=n_j)
+        mel[:, j] = 0
+        return mel
